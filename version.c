ea8cd113b1ef1542c23a1f1eb12d081fe3899b2b
Yvan05 <61843391+Yvan05@users.noreply.github.com>
2020-03-19 18:15:13 -0700

    estructura de mi programa

--- version.c
+++ version.c
@@ -1,1 +1,28 @@
 //programa en c
+//LICY_P06
+//16/03/2020
+//YAEL IVAN LINARES CRUZ
+/*
+Realizar un programa que contenga funciones que realice lo siguiente.
+1.- Leer una cadena, desplegar la cadena en forma : original, en
+mayúsculas, minúsculas, al revez. Cantidad de caracteres, Cantidad de
+vocales, consonantes y espacios o caracteres especiales.
+2.- Leer una cadena y desplegarla de la siguiente manera:
+(Realizar una función para cada salida)
+cadena: Ensenada
+
+*/
+
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+//prototipos
+void menu (void);
+void cadena_1(char *frase);
+void cadena_2(char *frase,char *frase_rev);
+//funcion principa;l
+int main (void)
+{
+   menu();
+   return 0;
+}
